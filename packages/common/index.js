dac0743fce452a10aa9b3a505b5fcd888c16aed6
yongzhan <516855237@qq.com>
2021-03-28 11:10:06 +0800

    comon and server changed

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,3 +1,4 @@
 module.exports = ()=>{
     console.log("hello from common")
+    console.log("hello from common")
 }
--- packages/server/index.js
+++ packages/server/index.js
@@ -2,3 +2,4 @@ const CommonFunction = require('@ngfed/common');
 
 CommonFunction()
 CommonFunction()
+CommonFunction()
